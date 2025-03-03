f49b41aaffc1dd137a91a8982ef4510b68bcf91b
tgabriel22 <thierrygabriel87@gmail.com>
2025-03-02 19:49:52 -0500

    Implementation of the speciality section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
+import SpecialityMenu from '../components/SpecialityMenu'
 
 const Home = () => {
   return (
     // <div>Home</div>
-    <div><Header/></div>
+    <div>
+      <Header/>
+      <SpecialityMenu/>
+    </div>
   )
 }
 
