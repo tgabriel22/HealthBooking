dd6195d2833942796fc0b989a7a61f5916346e6d
tgabriel22 <thierrygabriel87@gmail.com>
2025-03-02 19:23:36 -0500

    Done Implementing the header section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Header from '../components/Header'
 
 const Home = () => {
   return (
-    <div>Homepage</div>
+    // <div>Home</div>
+    <div><Header/></div>
   )
 }
 
