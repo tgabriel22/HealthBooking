fdc6548b8d609fe7227485a9fffde5df1b072bce
tgabriel22 <thierrygabriel87@gmail.com>
2025-03-03 17:02:57 -0500

    Done implementing Top Doctors section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../components/Header'
 import SpecialityMenu from '../components/SpecialityMenu'
+import TopDoctors from '../components/TopDoctors'
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
     <div>
       <Header/>
       <SpecialityMenu/>
+      <TopDoctors/>
     </div>
   )
 }
